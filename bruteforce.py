64e6b0ceb19503dd022a4a7a93afb4f083255eda
Daraniel <daraniel1000@gmail.com>
2019-11-25 20:19:02 +0100

    bfs fixed into an actual bfs + if distance smaller, ignores visited

--- bruteforce.py
+++ bruteforce.py
@@ -5,21 +5,28 @@ visited = []
 tovisit = []
 
 
-def bfs(node: int, dist: int):
-    print(node)
-    tovisit.pop(0)
-    if graph.nodes[node]['dist'] == 0:
-        graph.nodes[node]['dist'] = dist
-    else:
-        graph.nodes[node]['dist'] = min(graph.nodes[node]['dist'], dist)
-    if visited[node] == 0:
+def bfs(node: int):
+    tovisit.append((node, node))
+    prev_node : int
+    graph.add_edge(node, node, distance=0)
+    while len(tovisit) > 0:
+        prev_node = tovisit[0][0]
+        node = tovisit[0][1]
+        tovisit.pop(0)
+        if graph.nodes[node]['dist'] == 0:
+            graph.nodes[node]['dist'] = graph.nodes[prev_node]['dist'] + graph[prev_node][node]['distance']
+        else:
+            if graph.nodes[prev_node]['dist'] + graph[prev_node][node]['distance'] < graph.nodes[node]['dist']:
+                graph.nodes[node]['dist'] = graph.nodes[prev_node]['dist'] + graph[prev_node][node]['distance']
+            else:
+                continue
         visited[node] = 1
         for nbr in graph[node]:
             if visited[nbr] == 0:
-                tovisit.append((nbr, node))
-    if len(tovisit) > 0:
-        bfs(tovisit[0][0], graph.nodes[tovisit[0][1]]['dist'] + graph[tovisit[0][0]][tovisit[0][1]]['distance'])
-
+                tovisit.append((node, nbr))
+            else:
+                if graph.nodes[node]['dist'] + graph[node][nbr]['distance'] < graph.nodes[nbr]['dist']:
+                    tovisit.append((node, nbr))
 
 fp = open("out.txt", "r")
 line = fp.readline()
@@ -36,7 +43,7 @@ for n in range(graph.size()):
     visited.append(0)
     graph.add_node(n, dist=0)
 
-tovisit.append((0, 0))
+#tovisit.append((0, 0))
 print(graph.edges)
-bfs(start, 0)
+bfs(start)
 print(graph.nodes[end]['dist'])
